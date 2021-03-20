ec42fead97758098fd75600f969f559f873a28f8
kotaewing <kota.ewing@gmail.com>
2021-03-20 13:55:08 -0600

    initial game setup

--- assets/js/game.js
+++ assets/js/game.js
@@ -11,7 +11,12 @@ var enemyAttack = 12;
 
 var fight = function() {
     window.alert("Welcome to Robot Gladiators!");
+    var promptFight = window.prompt("Would you like to FIGHT or SKIP this battle? Enter 'FIGHT' or 'SKIP' to choose.");
+    console.log(promptFight);
 
+    // If player choses to fight, then fight
+    if (promptFight === "fight" || promptFight === "FIGHT") {
+    
     // subtract the value of 'playerAttack' from the value of 'enemyHealth' and use that result to update the value in the 'enemyHealth' variable
     enemyHealth = enemyHealth - playerAttack;
     // Log a resulting message to the console so we know that it worked.
@@ -41,6 +46,12 @@ var fight = function() {
     else {
         window.alert(playerName + " still has " + playerHealth + " health left.");
     }
+    // if player choses to skip
+} else if (promptFight === "skip" || promptFight === "SKIP") {
+    window.alert(playerName + " has chosen to skip the fight!");
+} else {
+    window.alert("You need to choose a valid option. Try again!");
+}
 };
 
 fight();
